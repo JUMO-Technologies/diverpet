aeef13c9e0f64021c7bd99a48728b9817ad8c4e7
Juan Manuel Ojeda <jojeda@jumotech.com>
2022-11-08 17:07:06 +0100

    product_price_list

---
+++ product_pricelist_prices/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "product_pricelist_prices",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.ictstudio.eu",
+    'category': 'Sales Management',
+    'version': '0.1',
+    'depends': ['base','product'],
+    'data': [
+        'views/product_pricelist.xml',
+        'views/product_product.xml',
+        'views/product_template.xml',
+        'views/templates.xml',
+    ],
+
+    'installable': True,
+}
