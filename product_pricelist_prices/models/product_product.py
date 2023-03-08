7dcfd648f127f7c8c3c75bfe5d74837cc9c79641
daromero2 <daromerogeigel@gmail.com>
2023-03-08 15:07:02 -0500

    update pricelist

--- product_pricelist_prices/models/product_product.py
+++ product_pricelist_prices/models/product_product.py
@@ -4,27 +4,8 @@ from odoo import models, fields, api, _
 
 _logger = logging.getLogger(__name__)
 
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def get_pricelists(self):
-        for rec in self:
-            pricelist_ids = self.env['product.pricelist'].search([('show_on_products', '=', True)])
-            details = []
-            for pricelist in pricelist_ids:
-                price = pricelist._get_product_price(rec, 1.0)
-                l_actuales = rec.pricelists.filtered(lambda x: x.pricelist_id.id == pricelist.id)
-                #self.env['product.pricelist.detail'].search([('product_id', '=', rec.id), ('pricelist_id', '=', pricelist.id)])
-                if l_actuales:
-                    l_actuales.write({'price': price})
-                else:
-                    l = self.env['product.pricelist.detail'].create(
-                        {'pricelist_id': pricelist.id, 'product_id': rec.id, 'price': price})
-                    details.append((4,l.id))
-            rec.write({'pricelists': details})
-
-    pricelists = fields.One2many(
-            'product.pricelist.detail',
-            string="Pricelists",
-            compute="get_pricelists", readonly=False
-    )
+    pricelist_ids = fields.One2many('product.pricelist.item', 'product_id')
--- product_pricelist_prices/models/product_template.py
+++ product_pricelist_prices/models/product_template.py
@@ -8,42 +8,9 @@ from odoo import models, fields, api, _
 
 _logger = logging.getLogger(__name__)
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    pricelist_ids = fields.One2many('product.pricelist.item', 'product_tmpl_id')
 
-    def get_pricelists(self):
-        for rec in self:
-            pricelist_ids = self.env['product.pricelist'].search([('show_on_products', '=', True)])
-            details = []
-            if not len(rec.product_variant_ids) > 1:
-                for pricelist in pricelist_ids:
-                    price = pricelist._get_product_price(rec, 1.0)
-                    #l_actuales = self.env['product.pricelist.detail'].search([('product_tmpl_id', '=', rec.id), ('pricelist_id', '=', pricelist.id)])
-                    l_actuales = rec.pricelists.filtered(lambda x: x.pricelist_id.id == pricelist.id)
-                    if l_actuales:
-                        l_actuales.write({'price': price})
-                    else:
-                        l = self.env['product.pricelist.detail'].create({'pricelist_id': pricelist.id, 'product_tmpl_id': rec.id, 'price': price})
-                        details.append((4,l.id))
-            rec.write({'pricelists': details})
-
-    def _get_pricelists(self):
-        self.pricelists = self.env['product.pricelist'].search(
-                [
-                    ('show_on_products', '=', True)
-                ]
-        )
-
-
-    def _set_pricelists(self):
-        for pricelist in self.pricelists:
-            if pricelist.product_price:
-                _logger.debug("Updating Price: %s", pricelist.product_price)
-                pricelist.price_set(self, pricelist.product_price)
-
-    pricelists = fields.One2many(
-            'product.pricelist.detail',
-            string="Pricelists",
-            compute="get_pricelists", readonly=False
-    )
